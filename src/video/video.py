b627661122d016524fdd5a5c2cbe9d25c89c39c2
maoxin <maoxin.horizon@gmail.com>
2018-11-14 02:34:14 +0800

    fix some small bugs

--- src/video/video.py
+++ src/video/video.py
@@ -241,11 +241,11 @@ class ValidatorSingleBusFGMaskImage(object):
                         
                         img = cv2.drawContours(sb.image_single_object.copy(), [hull], -1, (0, 255, 0), 3)
                         cv2.circle(img, (result['front_point'][0] - x0, result['front_point'][1] - y0),
-                                   30, (255, 0, 0), -1)
+                                   10, (255, 0, 0), -1)
                         cv2.circle(img, (result['back_point'][0] - x0, result['back_point'][1] - y0),
-                                   30, (255, 0, 0), -1)
+                                   10, (255, 0, 0), -1)
                         cv2.circle(img, (result['width_point'][0] - x0, result['width_point'][1] - y0),
-                                   30, (255, 0, 0), -1)
+                                   10, (255, 0, 0), -1)
                         axes[i].imshow(img)
                         axes[i].set_title(f"frame: {key}")
                     
